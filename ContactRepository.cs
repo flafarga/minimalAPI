239a5e7a0a7472978b7982b4b08ed34626a4df2a
Fran <francisco.lafarga@hotmail.com>
2022-07-30 19:10:28 +0200

    Add repository object and injection

---
+++ ContactRepository.cs
@@ -0,0 +1,13 @@
+using System.Text.Json;
+
+public class ContactRepository
+{
+    public IEnumerable<Contact>? Get()
+    {
+        return JsonSerializer
+        .Deserialize<IEnumerable<Contact>>(
+            File.OpenRead("wwwroot/contacts.json")
+        );
+    }
+
+}
